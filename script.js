0deaf953029957139d823ae2e2ea8a6b19c96862
Mohammed Sobhani <syedmahmoud41@gmail.com>
2024-08-14 22:09:23 +0600

    js function to get gif done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let apiKey="";
+let apiUrl=`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=smile&lang=en&rating=g`;
+let wrapper=document.querySelector(".wrapper");
+let searchBtn= document.getElementById("search-btn");
+function getGif(){
+    fetch(apiUrl)
+    .then(res=>res.json())
+    .then(item =>{
+    console.log(item.data[1].images.original.url)
+    let image = document.createElement("img");
+    image.src = item.data[7].images.original.url;
+    wrapper.append(image);
+}
+)
+.catch(err=>console.log(err));
+};
+searchBtn.addEventListener("click",getGif);
