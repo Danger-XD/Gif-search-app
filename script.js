5ac501b3a2baff195908ac70a790e0b4acdc962e
Mohammed Sobhani <syedmahmoud41@gmail.com>
2024-08-20 16:26:24 +0600

    project completed

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const searchBtn = document.getElementById("search-btn");
-const loader = document.querySelector(".loader");
 const searchBox = document.querySelector(".search-box");
+
 function getGif() {
   const searchText = document.getElementById("search-text");
-  const apiKey = "";
-  let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(searchText.value)}&lang=en&rating=r&limit=24`;
+  const apiKey = "dBD6nL4ML4lvNN0uH2O28XrdqWKWVNMN";
+  let gifLimit = 24;
+  let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(
+    searchText.value
+  )}&lang=en&rating=r&limit=${gifLimit}&offset=0`;
+  const loader = document.querySelector(".loader");
   const wrapper = document.querySelector(".wrapper");
+  loader.style.display="block";
+  wrapper.style.display="none";
+
+  wrapper.innerHTML = "";
   fetch(apiUrl)
     .then((res) => res.json())
     .then((item) => {
@@ -14,33 +22,38 @@ function getGif() {
       // console.log(apiUrl);
       // console.log(item.data[7].images.fixed_width.url);
       const gifs = item.data;
-      gifs.forEach(gif =>{
-        const gifUrl = gif.images.original.url;
+      gifs.forEach((gif) => {
         // console.log(gifUrl);
         let imgContainer = document.createElement("div");
-        imgContainer.className = ("img-container");
+        imgContainer.className = "img-container";
         let image = document.createElement("img");
-        image.className = ("gif-image");
-        image.src = gifUrl;
-        wrapper.append(imgContainer);
+        image.className = "gif-image";
+        const gifUrl = gif.images.original.url;
+        image.setAttribute("src",gifUrl);
+        image.onload = () => {
+          gifLimit--;
+          if(gifLimit == 0){
+            loader.style.display="none";
+            wrapper.style.display="grid";
+          }
+        };
         imgContainer.append(image);
-        function getUrl(){
+        wrapper.append(imgContainer);
+        function getUrl() {
           console.log(gifUrl);
           navigator.clipboard.writeText(gifUrl);
           alert("gif-URL has been copied to your clipboard.");
         }
-        imgContainer.addEventListener("click",getUrl);
-      })
+        imgContainer.addEventListener("click", getUrl);
+      });
     })
     .catch((err) => console.log(err));
 }
 searchBtn.addEventListener("click", getGif);
-window.addEventListener("load",loaderGif);
-function loaderGif(){
-  setTimeout(()=>{
-    loader.style.display = "none";
-    searchBox.style.display = "flex";
-  },3000)
-  
-}
-clearInterval()
+window.addEventListener("load", getGif);
+// function loaderGif() {
+//   setTimeout(() => {
+//     loader.style.display="none";
+//     wrapper.style.display ="block";
+//   }, 3000);
+// }
